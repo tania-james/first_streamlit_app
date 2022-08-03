f58db45782fc7b820229196f93f0581ee4f0e389
tania-james <110521504+tania-james@users.noreply.github.com>
2022-08-03 12:54:44 +0530

    Update streamlit_app.py
    Adding data from csv file

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,7 @@ streamlit.text ('=> 🍞 Puri Masala')
 streamlit.text ('=> 🍞 Chapathi-Veg. kurma')
 streamlit.text ('=> Tea/Coffee/Cold coffee')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
